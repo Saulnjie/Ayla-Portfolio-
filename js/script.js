3f4138f1c9c1e28824603782dc91ed0d9952d5a2
saul njie <saulus92@gmail.com>
2021-04-14 18:41:08 +0200

    started on hamburger menu

--- js/script.js
+++ js/script.js
@@ -16,3 +16,16 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+
+
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
