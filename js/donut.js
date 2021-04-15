e8aee274a0e8c3584fd889a7346968189e484233
saul njie <saulus92@gmail.com>
2021-04-15 20:35:59 +0200

    add feature to toggle between images

--- js/donut.js
+++ js/donut.js
@@ -1,6 +1,15 @@
 var acc = document.getElementsByClassName("shipping-price-container");
 var i;
 
+const donutImages = {
+  black: "img/xefAuoHA.jpeg",
+  beige: "img/9-G7b57w.jpeg"
+}
+
+const donutImage = document.querySelector(".donut-image");
+
+donutImage.src = "img/xefAuoHA.jpeg"
+
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
@@ -16,3 +25,17 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+
+
+const donutToggleButtons = document.querySelectorAll(".donut-color-toggle");
+
+donutToggleButtons.forEach(toggle => {
+  toggle.addEventListener("click", () => {
+    if (toggle.classList.contains("donut-color-toggle-beige")) {
+      donutImage.src = donutImages.beige
+    } else {
+      donutImage.src = donutImages.black;
+    }
+  })
+})
