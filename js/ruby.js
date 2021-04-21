4052aedf890f30fbe1843780a6c4849d80ed0b4a
SAUL NJIE <SAULUS92@gmail.com>
2021-04-21 20:21:41 +0200

    FINAL

--- js/ruby.js
+++ js/ruby.js
@@ -1,40 +1,36 @@
 var acc = document.getElementsByClassName("shipping-price-container");
 var i;
 
-const donutImages = {
-  black: "img/xefAuoHA.jpeg",
-  beige: "img/9-G7b57w.jpeg"
-}
+let activeImageIndex = 0;
 
-const donutImage = document.querySelector(".donut-image");
+const slideShowImages = document.querySelectorAll(".ruby-container-right img");
 
-donutImage.src = "img/xefAuoHA.jpeg"
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
-
-const donutToggleButtons = document.querySelectorAll(".donut-color-toggle");
-
-donutToggleButtons.forEach(toggle => {
-  toggle.addEventListener("click", () => {
-    if (toggle.classList.contains("donut-color-toggle-beige")) {
-      donutImage.src = donutImages.beige
-    } else {
-      donutImage.src = donutImages.black;
-    }
-  })
+slideShowImages.forEach((image, index) => {
+  if (index !== activeImageIndex) {
+    console.log(image)
+    image.style.display = "none";
+  }
 })
+
+function cycleImages(direction) {
+  slideShowImages[activeImageIndex].style.display = "none";
+
+  if (direction === "next") {
+    if (activeImageIndex + 1 >= slideShowImages.length) {
+      activeImageIndex = 0;
+    } else {
+      activeImageIndex++;
+    }
+  } else {
+    if (activeImageIndex - 1 < 0) {
+      activeImageIndex = slideShowImages.length - 1;
+    } else {
+      activeImageIndex--;
+    }
+  }
+
+  slideShowImages[activeImageIndex].style.display = "block";
+}
+
+
+
